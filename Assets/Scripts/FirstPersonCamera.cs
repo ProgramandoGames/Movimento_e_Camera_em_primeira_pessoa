2fefa7fed05672e5cff560b5ab0f2e920541f02f
Jair Bortolucci Júnior <jaibortolucci@gmail.com>
2020-07-16 09:17:07 -0300

    Files
    All files.

---
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour {
+
+    public Transform characterBody;
+    public Transform characterHead;
+
+    float sensitivityX = 1.0f;
+    float sensitivityY = 1.0f;
+
+    float rotationX = 0;
+    float rotationY = 0;
+
+    float angleYmin = -90;
+    float angleYmax = 90;
+
+    float smoothRotx = 0;
+    float smoothRoty = 0;
+
+    float smoothCoefx = 0.05f;
+    float smoothCoefy = 0.05f;
+
+    void Start() {
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    private void LateUpdate() {
+        transform.position = characterHead.position;
+    }
+
+    void Update() {
+
+        float verticalDelta = Input.GetAxisRaw("Mouse Y") * sensitivityY;
+        float horizontalDelta = Input.GetAxisRaw("Mouse X") * sensitivityX;
+
+        smoothRotx = Mathf.Lerp(smoothRotx, horizontalDelta, smoothCoefx);
+        smoothRoty = Mathf.Lerp(smoothRoty, verticalDelta, smoothCoefy);
+
+        rotationX += smoothRotx;
+        rotationY += smoothRoty;
+
+        rotationY = Mathf.Clamp(rotationY, angleYmin, angleYmax);
+
+        characterBody.localEulerAngles = new Vector3(0, rotationX, 0);
+
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    CharacterController controller;
+
+    Vector3 forward;
+    Vector3 strafe;
+    Vector3 vertical;
+
+    float forwardSpeed = 6f;
+    float strafeSpeed = 6f;
+
+    float gravity;
+    float jumpSpeed;
+    float maxJumpHeight = 2f;
+    float timeToMaxHeight = 0.5f;
+
+    void Start() {
+
+        controller = GetComponent<CharacterController>();
+
+        gravity = (-2 * maxJumpHeight) / (timeToMaxHeight * timeToMaxHeight);
+        jumpSpeed = (2 * maxJumpHeight) / timeToMaxHeight;
+
+    }
+
+    void Update() {
+
+
+        float forwardInput = Input.GetAxisRaw("Vertical");
+        float strafeInput = Input.GetAxisRaw("Horizontal");
+
+        // force = input * speed * direction
+        forward = forwardInput * forwardSpeed * transform.forward;
+        strafe = strafeInput * strafeSpeed * transform.right;
+
+        vertical += gravity * Time.deltaTime * Vector3.up;
+
+        if(controller.isGrounded) {
+            vertical = Vector3.down;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && controller.isGrounded) {
+            vertical = jumpSpeed * Vector3.up;
+        }
+
+        if (vertical.y > 0 && (controller.collisionFlags & CollisionFlags.Above) != 0) {
+            vertical = Vector3.zero;
+        }
+
+        Vector3 finalVelocity = forward + strafe + vertical;
+
+        controller.Move(finalVelocity * Time.deltaTime);
+
+
+    }
+
+}
